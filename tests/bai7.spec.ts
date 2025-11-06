b9da46fa7931467a4d7d593cae1b1150ff926042
phuongdung1402 <60888807+phuongdung1402@users.noreply.github.com>
2025-11-06 10:49:46 +0700

    up not code bai 7

--- tests/bai7.spec.ts
+++ tests/bai7.spec.ts
@@ -116,6 +116,8 @@ test('Ví dụ về alert (Confirm - Prompt)', async ({page})=> {
 
  })
 
+
+ // Locator chain -> mình có thể nối nhiều locator lại với ( tìm từ thằng cha trỏ xuống thằng con , chứ ko cần tìm trong cả page)
 test('Ví dụ về modal', async({page})=> {
     await page.goto('https://demoapp-sable-gamma.vercel.app/')
     await page.getByRole('link', {name:'Bài 4: Mouse Actions'}).click()
