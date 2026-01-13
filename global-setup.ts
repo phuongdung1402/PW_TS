ef09841ac04e4db96ca803d22ed1d5d9963096b5
phuongdung1402 <phdung1402@gmail.com>
2026-01-13 07:27:54 +0700

    up 13/1/26

--- global-setup.ts
+++ global-setup.ts
@@ -1,13 +1,36 @@
-import { FullConfig} from 'playwright/test'
+// import { FullConfig} from 'playwright/test'
 
-async function globalSetup (config : FullConfig) {
-    console.log(`[GLOBAL SETUP] bắt đầu khởi động hệ thống`);
+// async function globalSetup (config : FullConfig) {
+//     console.log(`[GLOBAL SETUP] bắt đầu khởi động hệ thống`);
+//     await new Promise((r)=> setTimeout(r, 1000));
+//     process.env.DB_CONNECTION_URL = 'postgress://admin:123';
+//     process.env.API_PORT = '8080';
+// }
 
-    await new Promise((r)=> setTimeout(r, 1000));
-    process.env.DB_CONNECTION_URL = 'postgress://admin:123';
-    process.env.API_PORT = '8080';
-    
+// export default globalSetup;
+
+
+import {chromium, FullConfig} from '@playwright/test'
+
+async function globalSetup(config: FullConfig){
+    const browser = await chromium.launch({headless: false});
+    const newContext = await browser.newContext();
+    const page  = await newContext.newPage();
+
+    try {
+        await page.goto('https://github.com/login');
+        await page.click('input[name="commit-sign-in"]', {timeout: 5000});
+
+        await page.context().storageState({path: 'auth.json'})
+
+    }catch(error){
+        console.log('Login that bai!');
+
+        throw error;
+    } finally {
+        await browser.close();
+    }
+
 }
 
 export default globalSetup;
-
---
+++ playwright.lessons.config.ts
@@ -0,0 +1,85 @@
+import { defineConfig, devices } from '@playwright/test';
+import DotenvFlow from 'dotenv-flow';
+
+if(!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development';
+
+}
+
+DotenvFlow.config({
+  default_node_env: 'development',
+});
+
+
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  //testMatch: '**/*'
+  //testDir: './homeworks',
+  testDir: './tests',
+  //Khai bao global setup & teardown
+  globalSetup: './global-setup.ts',
+  globalTeardown: './global-teardown.ts',
+  //testMatch: '**/*.spec.ts',
+  /* Run tests in files in parallel */
+  
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  //timeout : 17000,
+  //timeout : 20000,
+
+  use: {
+    baseURL :'https://crm.anhtester.com',
+    //actionTimeout: 15000,
+    headless: true,
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  // expect: {
+  //   timeout: 7000
+  // },
+
+  /* Configure projects for major browsers */
+  projects: [
+
+    {
+      name: 'setup',
+      testMatch: '**/*.setup.ts',
+    },
+
+  
+    {
+      name: 'chromium',
+      use : {
+        ...devices['Desktop Chrome'],
+      },
+      dependencies: ['setup']
+    },
+
+  ],
+
+ 
+});
