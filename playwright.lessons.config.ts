3ae1010b3bcb30b7f353b592537d8719bf586d85
phuongdung1402 <phdung1402@gmail.com>
2026-01-15 07:32:02 +0700

    up1501

--- playwright.lessons.config.ts
+++ playwright.lessons.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -48,7 +48,7 @@ export default defineConfig({
   //timeout : 20000,
 
   use: {
-    baseURL: 'https://www.saucedemo.com/inventory.html',
+    baseURL: 'https://www.saucedemo.com',
     //actionTimeout: 15000,
     headless: false,
     /* Base URL to use in actions like `await page.goto('')`. */
--- tests/Cookies/shop.spec.ts
+++ tests/Cookies/shop.spec.ts
@@ -3,12 +3,19 @@ import {test, expect} from '@playwright/test';
 
 //test.use({storageState: {cookies: [], origins: []}})
 
+test.use({
+    video: {
+        mode:'retain-on-failure',
+        size: {width: 1280, height: 720},
+    }
+})
+
 
 test('Mua hàng (Đã login sẵn)', async ({page})=> {
     console.log('[TEST] Bắt đầu test mua hàng .....');
 
     //Vào thẳng trang chủ (Sẽ tự nhận cookies từ user.json)
-    await page.goto('/inventory.html');
+    await page.goto('https://www.saucedemo.com/inventory.html');
     //await page.pause()
 
     //Assert : Chắc chắn là đã login rồi (Có nút Add to cart)
